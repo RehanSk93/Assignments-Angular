497eca9b5ac7d4ef0c0c3d7791678d4660cd4721
RehanSk93 <skazam1993@gmail.com>
2019-08-23 18:46:27 +0530

    Few changes have done

--- angularLifecycleHooks/src/app/app.component.ts
+++ angularLifecycleHooks/src/app/app.component.ts
@@ -27,10 +27,8 @@ export class AppComponent implements OnInit {
 //    1. ngOnChanges          -> Called after a bound input property changes.
 //    2. ngOnInit             -> Called once the component is initialized.
 //    3. ngDoCheck            -> Called during every change detection run.
-//    4. ngAfterContentInit   -> Called every time the projected content has    //                               been checked. 
-//    5. ngAfterViewInit      -> Called after the component's view(and child //
-//                               views) have been initialized.
-//    6. ngAfterViewChecked   -> Called every time the view(and child view) 
-//                               have been checked.
-//    7. ngOnDestroy          -> Called once the component is about to be 
-//                               destroyed.
+//    4. ngAfterContentInit   -> Called every time the projected content has been checked. 
+//    5. ngAfterViewInit      -> Called after the component's view(and child views) have been
+//                               initialized.
+//    6. ngAfterViewChecked   -> Called every time the view(and child view) have been checked.
+//    7. ngOnDestroy          -> Called once the component is about to be destroyed.
--- ngCalculator/src/app/calculator/calculator.component.ts
+++ ngCalculator/src/app/calculator/calculator.component.ts
@@ -32,7 +32,6 @@ export class CalculatorComponent implements OnInit {
     }else{
       this.result += value;
     }
-    
   }
 
   constructor() { }
